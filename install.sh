9b866318536dff639c93795951c64cdd237d7ce8
Leandro Silva <grafluxe@users.noreply.github.com>
2021-11-03 01:22:54 -0400

    Add quick-install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Author: Leandro Silva (http://grafluxe.com)
+#Copyright: 2021 Leandro Silva
+#License: MIT
+
+version="1.1.0"
+
+curl -Lko to-sh.zip https://github.com/Grafluxe/to.sh/archive/v$version.zip
+unzip to-sh.zip
+sh to.sh-$version/to-setup.sh
+source ~/.profile
+source ~/.zshrc
+echo Cleaning up...
+rm -fr to.sh-$version
+rm to-sh.zip
+echo Done!
